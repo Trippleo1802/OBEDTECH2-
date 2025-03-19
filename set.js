afcad7d9a826dc1fb81cdb0043b69cc664b40ec5
caseyweb <caseywebsthedonn25@gmail.com>
2025-03-19 08:16:37 +0300

    ᴄᴀsᴇʏʀʜᴏᴅᴇs xᴍᴅ

--- set.js
+++ set.js
@@ -1,4 +1,3 @@
-
 const fs = require('fs-extra');
 const { Sequelize } = require('sequelize');
 if (fs.existsSync('set.env'))
@@ -10,12 +9,18 @@ const DATABASE_URL = process.env.DATABASE_URL === undefined
     : process.env.DATABASE_URL;
 module.exports = { session: process.env.SESSION_ID || '',
     PREFIXE: process.env.PREFIX || ".",
-    OWNER_NAME: process.env.OWNER_NAME || "Obed tech",
-    NUMERO_OWNER : process.env.NUMERO_OWNER || " Obed tech",              
+    OWNER_NAME: process.env.OWNER_NAME || "CASEYRHODES ⚜️",
+    CAPTION : process.env.CAPTION || "CASEYRHODES-XMD",
+    NUMERO_OWNER : process.env.NUMERO_OWNER || "Caseyrhodes",              
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "yes",
+    AUTO_REACT : process.env.AUTO_REACT || 'yes',
+    ANTICALL: process.env.ANTICALL || 'yes',
+    GURL: process.env.GURL  || "https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E",
+    WEBSITE :process.env.GURL || "https://whatsapp.com/channel/0029VakUEfb4o7qVdkwPk83E",
     AUTO_DOWNLOAD_STATUS: process.env.AUTO_DOWNLOAD_STATUS || 'no',
-    BOT : process.env.BOT_NAME || 'Obed tech',
-    URL : process.env.BOT_MENU_LINKS || 'https://files.catbox.moe/1khsx4.jpg',
+    BOT : process.env.BOT_NAME || 'CASEYRHODES XMD',
+    URL : process.env.BOT_MENU_LINKS || 'https://telegra.ph/file/17c83719a1b40e02971e4.jpg',
+    URL: process.env.URL || "https://i.ibb.co/3YVC86md/lordcasey.jpg",
     MODE: process.env.PUBLIC_MODE || "yes",
     PM_PERMIT: process.env.PM_PERMIT || 'yes',
     HEROKU_APP_NAME : process.env.HEROKU_APP_NAME,
@@ -24,10 +29,9 @@ module.exports = { session: process.env.SESSION_ID || '',
     ETAT : process.env.PRESENCE || '',
     CHATBOT : process.env.PM_CHATBOT || 'no',
     DP : process.env.STARTING_BOT_MESSAGE || "yes",
-    ADM : process.env.ANTI_DELETE_MESSAGE || 'no',
-    ANTICALL : process.env.ANTICALL || 'no',
-                  AUTO_REACT_STATUS : process.env.AUTO_REACT_STATUS || 'yes',
-                  AUTO_READ : process.env.AUTO_READ || 'yes',
+    AUTOREAD_MESSAGE : process.env.AUTO_READ || "yes",
+    DP : process.env.STARTING_BOT_MESSAGE || "yes",
+    TECH : process.env.AUTO_REACT_STATUS || 'yes',
     DATABASE_URL,
     DATABASE: DATABASE_URL === databasePath
         ? "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway" : "postgresql://postgres:bKlIqoOUWFIHOAhKxRWQtGfKfhGKgmRX@viaduct.proxy.rlwy.net:47738/railway",
